c7b2b24cfc46ce73b13f06d3f02c03de1627cad2
designhenriqfp-byte <designhenriqfp@gmail.com>
2026-04-08 21:54:57 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-=======
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
->>>>>>> fd159b82f21ac7bd217f5e6aabbae44f0cfc860b
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -18,22 +13,4 @@ const App = () => {
   );
 };
 
-<<<<<<< HEAD
 export default App;
-=======
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
->>>>>>> fd159b82f21ac7bd217f5e6aabbae44f0cfc860b
