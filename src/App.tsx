7d6726c7e1d734d48d9f9e7ff924457c581e5ac4
pedrosantosdpaula334-rgb <pedrosantosdpaula334@gmail.com>
2026-04-08 21:11:00 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#000",
+        color: "#fff",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1>Site funcionando 🚀</h1>
+    </main>
+  );
+}
+
+export default App;
